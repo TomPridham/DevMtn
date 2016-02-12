69628daaeb7cecbe749736f2334f4dd4a54379a1
Tom Pridham <pridham.tom@gmail.com>
2016-02-11 22:01:44 -0700

    Added mini-paint project, jQuery2 project, and bower for libraries. Worked on prettifying jQuery2 project.

--- Week 2/mini-paint/app.js
+++ Week 2/mini-paint/app.js
@@ -1,40 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	var color = "white";
-		//selector //listener //callback
-	$('.box').on("click", function(){
-		$(this).addClass(color); // function body
-	})
+    var color = "white";
+    var colors = 'red blue green yellow white';
+    //selector //listener //callback
+    $('.box').on("click", function () {
+        $(this).addClass(color); // function body
+    })
 
-	$(".box").on('dblclick', function(){
-		$(this).removeClass(color);
-	})
+    $(".box").on('dblclick', function () {
+        $(this).removeClass(colors);
+    })
 
-	$('#reset').on('click', function(){
-		$('.box').removeClass('red blue green yellow white');
-	})
+    $('#reset').on('click', function () {
+        $('.box').removeClass(colors);
+    })
 
-	$('#red').on('click', function(){
-		color = 'red';
-	})
+    $('#red').on('click', function () {
+        color = 'red';
+    })
 
-	$('#blue').on('click', function(){
-		color = 'blue';
-	})
+    $('#blue').on('click', function () {
+        color = 'blue';
+    })
 
-	$('#green').on('click', function(){
-		color = 'green';
-	})
+    $('#green').on('click', function () {
+        color = 'green';
+    })
 
-	$('#yellow').on('click', function(){
-		color = 'yellow';
-	})
+    $('#yellow').on('click', function () {
+        color = 'yellow';
+    })
 
-	$('#white').on('click', function(){
-		color = 'white';
-	})
-
-
+    $('#white').on('click', function () {
+        color = 'white';
+    })
 
 
 })
